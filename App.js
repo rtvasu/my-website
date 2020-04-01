3c615afd86906b2c22aaa8557ddcae14051a4632
Aarti Vasudevan <44345358+rtvasu@users.noreply.github.com>
2020-04-01 17:05:59 -0400

    Added PreviewImage, About, Links, Posts components

--- App.js
+++ App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import aboutPic from './aboutPic.JPG';
+import Posts from './posts/posts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//make this reusable so that with different parameters, you can pass an image
+class PreviewImage extends React.Component {
+  render() {
+    return (
+      <img
+        src={aboutPic} 
+        alt='Aarti Vasudevan!' 
+        width='100%' 
+        height='auto'/>
+    )
+  }
 }
 
-export default App;
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Hi, I'm Aarti, a Computer Engineering undergraduate student from the University of Waterloo, and I love learning more about front-end systems everyday! My interests include learning how to use JavaScript (React) on webpages like this one, working on full-stack development in organizations, singing, and creating art. 
+        </p>
+        <p>
+          Looking for Fall 2020 internships!
+        </p>
+      </div>
+    )
+  }
+}
+
+class Links extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Links </h1>
+        <li>
+          <a 
+            href="https://github.com/rtvasu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+              GitHub
+          </a>
+        </li>
+        <li>
+          <a 
+            href="https://www.linkedin.com/in/aarti-vasudevan/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+              LinkedIn
+          </a>
+        </li>
+      </div>
+    )
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <PreviewImage />
+        <About />
+        <Links />
+        <Posts />
+    </div>
+    )
+  }
+}
