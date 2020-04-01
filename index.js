eb8568b815c0358ea62832f5dfc758ce3427aa15
Aarti Vasudevan <44345358+rtvasu@users.noreply.github.com>
2020-04-01 17:08:51 -0400

    Added Routes
    temporary, working on creating new web-page for Clock rather than it adding the component to the bottom of App

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App from './App'
+import { HashRouter, Route } from 'react-router-dom';
+import Clock from './posts/liveClock'
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, this is rtvasu!');
+//the program itself is allowed one render()
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(element, document.getElementById('root'));
+//picked HashRouter over Router since former has in-built hashHistory prop
+ReactDOM.render((
+    <HashRouter>
+      <Route path="/" component={App}/>
+      <Route path="/live-clock" component={Clock}/>
+    </HashRouter>
+  ), document.getElementById('root'));
+//credits end here
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
