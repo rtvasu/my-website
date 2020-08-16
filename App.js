ba32e843297197412efbe46d8d2750e912ab02f6
Aarti Vasudevan <44345358+rtvasu@users.noreply.github.com>
2020-08-15 21:41:11 -0400

    Renders Home

--- App.js
+++ App.js
@@ -1,73 +1,21 @@
-import React from 'react';
-import './App.css';
-import aboutPic from './aboutPic.JPG';
-import Posts from './posts/posts';
-
-//make this reusable so that with different parameters, you can pass an image
-class PreviewImage extends React.Component {
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Navigation from './Navigation';
+ 
+class App extends Component {
   render() {
     return (
-      <img
-        src={aboutPic} 
-        alt='Aarti Vasudevan!' 
-        width='100%' 
-        height='auto'/>
-    )
+       <HashRouter>
+        <div>
+          <Navigation />
+            <Switch>
+              <Route path="/" component={Home} exact/>
+           </Switch>
+        </div> 
+      </HashRouter>
+    );
   }
 }
-
-class About extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>
-          Hi, I'm Aarti, a Computer Engineering undergraduate student from the University of Waterloo, and I love learning more about front-end systems everyday! My interests include learning how to use JavaScript (React) on webpages like this one, working on full-stack development in organizations, singing, and creating art. 
-        </p>
-        <p>
-          Looking for Fall 2020 internships!
-        </p>
-      </div>
-    )
-  }
-}
-
-class Links extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1> Links </h1>
-        <li>
-          <a 
-            href="https://github.com/rtvasu"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-              GitHub
-          </a>
-        </li>
-        <li>
-          <a 
-            href="https://www.linkedin.com/in/aarti-vasudevan/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-              LinkedIn
-          </a>
-        </li>
-      </div>
-    )
-  }
-}
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <PreviewImage />
-        <About />
-        <Links />
-        <Posts />
-    </div>
-    )
-  }
-}
+ 
+export default App;
